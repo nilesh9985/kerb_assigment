9f296a6023413ae1ebe76c05d6a7f1caebc9cdd3
Nilesh Patil <nilesh.patil@ethoca.com>
2021-10-05 19:49:36 +0530

    assigment

---
+++ ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "server_port" {
+  description = "The port the web server will be listening"
+  type        = number
+  default     = 8080
+}
+
+variable "elb_port" {
+  description = "The port the elb will be listening"
+  type        = number
+  default     = 80
+}
+
+
+
